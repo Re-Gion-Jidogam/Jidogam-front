efb784725bca2bc847cb68304fc985af9db27901
cozy-ito <cozy.ito.dev@gmail.com>
2025-07-17 23:36:59 +0900

    initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,112 @@
+// eslint.config.js
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import eslintPluginImport from "eslint-plugin-import";
+import prettierPlugin from "eslint-plugin-prettier";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// next/core-web-vitals 및 next/typescript를 Flat config로 변환
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+export default tseslint.config(
+  { ignores: ["dist", "scripts"] },
 
-export default eslintConfig;
+  // Next.js core rules 적용
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    settings: {
+      react: { version: "detect" },
+    },
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "prettier/prettier": "error",
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "prefer-arrow-callback": ["error"],
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "react/destructuring-assignment": ["warn", "always"],
+      "react/function-component-definition": [
+        "error",
+        {
+          namedComponents: "function-declaration",
+          unnamedComponents: "arrow-function",
+        },
+      ],
+    },
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      prettier,
+    ],
+  },
+
+  {
+    plugins: {
+      import: eslintPluginImport,
+    },
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "object",
+          ],
+          pathGroups: [
+            {
+              pattern: "react",
+              group: "builtin",
+              position: "before",
+            },
+            {
+              pattern: "./pages/**",
+              group: "internal",
+              position: "before",
+            },
+            {
+              pattern: "./assets/**",
+              group: "internal",
+              position: "after",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["react"],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+          "newlines-between": "always",
+        },
+      ],
+    },
+  },
+);
