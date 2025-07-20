ca07d3164034873c50f2de3a08831cc96d12fb8c
cozy-ito <cozy.ito.dev@gmail.com>
2025-07-20 22:36:36 +0900

    🔧 Chore: 폰트 및 base, theme, components, utilities 적용

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+// tailwind.config.ts
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./src/styles/**/*.{css}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
