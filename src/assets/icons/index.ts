b4ea212e22307c5207b859631155bd4077cb781a
cozy-ito <cozy.ito.dev@gmail.com>
2025-07-27 21:16:10 +0900

    ✨ Feat: SVGIcon 컴포넌트 구현

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,6 @@
+export { default as CloseIcon } from "./btn-close.svg";
+export { default as HouseIcon } from "./house.svg";
+export { default as MapIcon } from "./map.svg";
+export { default as ProfileIcon } from "./profile.svg";
+export { default as SearchIcon } from "./search.svg";
+export { default as StampIcon } from "./stamp.svg";
