3443e57997d09e7f15790825aa251f8cfa379f03
hyobiin9 <moone1234@kakao.com>
2025-09-24 20:03:41 +0900

    🐛Fix: 페이지 레이아웃 테스트부분 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-import Toast from "@/components/Toast";
-
 export default function Home() {
-  return (
-    <div>
-      <Toast
-        type="NOT_MOVE"
-        title="햄버거에 미친 사람이 만든 전주 수제버거 맛집들"
-        message="에 추가됐어요"
-      />
-    </div>
-  );
+  return <h1>Hello Jidogam</h1>;
 }
