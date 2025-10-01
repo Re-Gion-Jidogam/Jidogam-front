d249d7ea16bbe836defd55429951f2088cdbf21d
hyobiin9 <moone1234@kakao.com>
2025-10-01 18:45:46 +0900

    ✨ Feat: Stamp Card 기본 레이아웃 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import StampCard from "@/components/StampCard";
+
 export default function Home() {
-  return <h1>Hello Jidogam</h1>;
+  return (
+    <div>
+      <StampCard {...placeCardData} />
+      <h1>Hello Jidogam</h1>
+    </div>
+  );
 }
+
+const placeCardData = {
+  storeName: "투썸플레이스 안산그랑시티자이점",
+  storeCategory: "카페",
+  storeScore: 2.8,
+  storeAddress: "경기 안산시 상록구 사동",
+  stampingNumber: 50,
+};
