72c5689a7d4e812a9c6f608f497d550d3df2f3f0
hyobiin9 <moone1234@kakao.com>
2025-10-01 17:06:59 +0900

    🐛 Fix: 메인 페이지의 테스트 부분 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,3 @@
-import PlaceCard from "@/components/PlaceCard";
-
 export default function Home() {
-  return (
-    <div>
-      <PlaceCard {...placeCardData} variant="bottom-button" />
-      <PlaceCard {...placeCardData} variant="stamp" />
-      <PlaceCard {...placeCardData} variant="stamp-disabled" />
-      <h1>Hello Jidogam</h1>
-    </div>
-  );
+  return <h1>Hello Jidogam</h1>;
 }
-
-const placeCardData = {
-  storeName: "투썸플레이스 안산그랑시티자이점",
-  storeCategory: "카페",
-  storeScore: 2.8,
-  storeAddress: "경기 안산시 상록구 사동",
-  stampingDay: "2025. 5. 14",
-  containingGuideBookNumber: "4,928",
-};
