db4c0958bc6b4bd5b6768bbb5bba627afc119140
hyobiin9 <moone1234@kakao.com>
2025-10-04 04:31:11 +0900

    💄 Style: 메인페이지 테스트코드 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,7 @@
-import StampCard from "@/components/StampCard";
-
 export default function Home() {
   return (
-    <div className="p-10 flex flex-col gap-4">
-      <StampCard placeInfo={placeCardData} variant="default" />
-      <StampCard placeInfo={placeCardData} variant="stamp" />
-      <StampCard placeInfo={placeCardData} variant="stamp-disabled" />
-      {/* <h1>Hello Jidogam</h1> */}
+    <div>
+      <h1>Hello Jidogam</h1>
     </div>
   );
 }
-
-const placeCardData = {
-  name: "투썸플레이스 안산그랑시티자이점",
-  category: "카페",
-  point: 2.8,
-  address: "경기 안산시 상록구 사동",
-  visitedDate: "2025. 5. 14",
-  guidebookCount: "",
-};
