3b52a0d79bd4c497742432a64f657c33d23abe10
cozy-ito <cozy.ito.dev@gmail.com>
2025-10-04 03:41:16 +0900

    🔨Refactor: 불필요한 코드 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,3 @@
-"use client";
-
-import ActionSheet from "@/components/ActionSheet";
-
-const menus = [
-  {
-    title: "sample",
-    onClick: () => {
-      console.log("sample1");
-    },
-  },
-  {
-    title: "sample2",
-    onClick: () => {
-      console.log("sample2");
-    },
-  },
-];
-
 export default function Home() {
-  return (
-    <h1>
-      Hello Jidogam
-      <div>
-        <ActionSheet actionSheetTitle="가이드북 카드 썸네일" menus={menus} />
-      </div>
-    </h1>
-  );
+  return <h1>Hello Jidogam</h1>;
 }
