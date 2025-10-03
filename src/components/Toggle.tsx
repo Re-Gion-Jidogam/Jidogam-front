bd13717375debf5dcbcb7c499e6af0c1878c44b1
hyobiin9 <moone1234@kakao.com>
2025-10-04 01:04:55 +0900

    🔨 Refactor: Toggle Wrapper 제거 후 Toggle 만 사용

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -7,26 +7,59 @@ import { motion } from "framer-motion";
 
 import SVGIcon from "./SVGIcon";
 
-interface ToggleWrapperProps {
+interface ToggleProps {
   initial?: boolean;
   label: string;
+  onChange?: (state: boolean) => void;
   disabled?: boolean;
 }
 
-interface ToggleProps {
-  initial?: boolean;
-  onChange?: (state: boolean) => void;
-  disabled: boolean;
-}
-
-export default function ToggleWrapper({
+export default function Toggle({
   initial = false,
   label,
+  onChange,
   disabled = false,
-}: ToggleWrapperProps) {
+}: ToggleProps) {
+  const [isOn, setIsOn] = useState(initial);
+
+  const handleToggle = () => {
+    if (disabled) return;
+
+    setIsOn(!isOn);
+    onChange?.(!isOn);
+  };
+
   return (
     <div className={clsx("flex items-center gap-2")}>
-      <Toggle initial={initial} disabled={disabled} />
+      <motion.button
+        onClick={handleToggle}
+        className={clsx(
+          "w-11 h-6 p-0.5 rounded-[100px]",
+          {
+            "bg-gray-600": !isOn,
+            "bg-primary-300": isOn && !disabled,
+            "bg-primary-400": isOn && disabled,
+          },
+          {
+            "cursor-pointer": !disabled,
+            "cursor-not-allowed": disabled,
+          },
+          {
+            "pointer-events-none": disabled,
+          },
+        )}
+      >
+        <motion.div
+          className={clsx(
+            "w-5 h-5 rounded-full shadow-[0_4px_20px_0_rgba(0,0,0,0.10)]",
+            {
+              "bg-gray-50": !disabled,
+              "bg-gray-300": disabled,
+            },
+          )}
+          animate={{ x: isOn ? 20 : 0 }}
+        />
+      </motion.button>
       <p
         className={clsx("font-medium text-sm", {
           "text-gray-900": !disabled,
@@ -38,48 +71,5 @@ export default function ToggleWrapper({
       </p>
       <SVGIcon icon="GrayGuideIcon" className="cursor-pointer" />
     </div>
-  );
-}
-
-function Toggle({ initial = false, onChange, disabled }: ToggleProps) {
-  const [isOn, setIsOn] = useState(initial);
-
-  const handleToggle = () => {
-    if (disabled) return;
-
-    setIsOn(!isOn);
-    onChange?.(!isOn);
-  };
-
-  return (
-    <motion.button
-      onClick={handleToggle}
-      className={clsx(
-        "w-11 h-6 p-0.5 rounded-[100px]",
-        {
-          "bg-gray-600": !isOn,
-          "bg-primary-300": isOn && !disabled,
-          "bg-primary-400": isOn && disabled,
-        },
-        {
-          "cursor-pointer": !disabled,
-          "cursor-not-allowed": disabled,
-        },
-        {
-          "pointer-events-none": disabled,
-        },
-      )}
-    >
-      <motion.div
-        className={clsx(
-          "w-5 h-5 rounded-full shadow-[0_4px_20px_0_rgba(0,0,0,0.10)]",
-          {
-            "bg-gray-50": !disabled,
-            "bg-gray-300": disabled,
-          },
-        )}
-        animate={{ x: isOn ? 20 : 0 }}
-      />
-    </motion.button>
   );
 }
