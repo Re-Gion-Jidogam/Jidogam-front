a140a2ea53100ded4a3e6ac694172b4bb99de916
cozy-ito <cozy.ito.dev@gmail.com>
2025-10-04 03:28:13 +0900

    ✨ Feat: ActionSheet 컴포넌트 구현

--- src/components/ActionSheet.tsx
+++ src/components/ActionSheet.tsx
@@ -1,33 +1,68 @@
 "use client";
 
-interface Menu {
-  title: string;
-  onClick: () => void;
-}
+import { ButtonHTMLAttributes, ReactNode } from "react";
+
+import clsx from "clsx";
 
 interface ActionSheetProps {
+  body?: ReactNode;
+  footer?: ReactNode;
   actionSheetTitle: string;
-  menus: Menu[];
+  onClickBackdrop?: () => void;
 }
 
-export default function ActionSheet({
+function ActionSheetContainer({
   actionSheetTitle,
-  menus,
+  body,
+  footer,
+  onClickBackdrop,
 }: ActionSheetProps) {
   return (
-    <div className="fixed inset-0 w-full px-3 pb-4 bg-gray-100">
-      <div className="absolute bottom-0 overflow-hidden bg-blue-200 rounded-xl">
-        <p className="py-3 bg-gray-200 text-xs font-semibold text-gray-900 text-center">
-          {actionSheetTitle}
-        </p>
-        <ul className="bg-gray-300">
-          {menus.map(({ title, onClick }) => (
-            <li key={title} className="p-3.5 border border-gray-300">
-              <button onClick={onClick}>{title}</button>
-            </li>
-          ))}
-        </ul>
+    <div className="fixed inset-0 px-3 text-gray-900 animate-fade-in">
+      <div
+        className="absolute inset-0 bg-[rgba(0,0,0,0.2)]"
+        onClick={onClickBackdrop}
+      />
+      <div className="absolute bottom-4 flex flex-col gap-2 w-[calc(100%-24px)]">
+        <div className="overflow-hidden bg-blue-200 rounded-xl">
+          <p className="py-3 bg-[rgba(255,255,255,0.9)] text-xs font-semibold text-gray-900 text-center">
+            {actionSheetTitle}
+          </p>
+          <ul className="bg-gray-0">{body}</ul>
+        </div>
+        <div className="overflow-hidden bg-gray-0 font-bold text-center rounded-xl">
+          {footer}
+        </div>
       </div>
     </div>
   );
 }
+
+function ActionSheetButton({
+  children,
+  className,
+  disabled,
+  ...props
+}: ButtonHTMLAttributes<HTMLButtonElement>) {
+  return (
+    <li
+      className={clsx(
+        "p-3.5 border-t border-gray-300 text-center",
+        "bg-[rgba(255,255,255,0.9)] text-gray-700",
+        "hover:bg-gray-0 hover:text-gray-900",
+        "cursor-pointer transition-colors",
+        className,
+      )}
+    >
+      <button className="cursor-pointer" disabled={disabled} {...props}>
+        {children}
+      </button>
+    </li>
+  );
+}
+
+const ActionSheet = Object.assign(ActionSheetContainer, {
+  Button: ActionSheetButton,
+});
+
+export default ActionSheet;
