4c67a12851134f0e91fdf47d0bfe3caeb3f52d26
hyobiin9 <moone1234@kakao.com>
2025-10-08 22:42:04 +0900

    ✨ Feat: PasswordResetStep 컴포넌트 개발

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,21 +1,3 @@
-"use client";
-
-import { useState } from "react";
-
-import { LoginStep } from "./_components/LoginStep";
-
 export default function Account() {
-  const [email, setEmail] = useState("example@example.com");
-  const [password, setPassword] = useState("");
-
-  return (
-    <LoginStep
-      email={email}
-      password={password}
-      onEmailChange={setEmail}
-      onPasswordChange={setPassword}
-      onLogin={() => console.log("Login!!")}
-      onPasswordReset={() => console.log("Reset!!")}
-    />
-  );
+  return <h1>Account Page</h1>;
 }
