22326746bb81e2a1fa10ad1fe692c13229f45d4a
hyobiin9 <moone1234@kakao.com>
2025-11-04 22:35:12 +0900

    ✨ Feat: onClick Event

--- src/app/cta/page.tsx
+++ src/app/cta/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import ctaBackground from "@/assets/icons/cta_background.png";
-import Button from "@/components/Button";
 
 export default function CTA() {
   return (
     <div className="fixed inset-0 w-screen h-screen overflow-hidden flex flex-col">
-      {/* 이미지 영역 - 상단이 잘리고 하단이 보이도록 */}
       <div className="relative w-full flex-1">
         <Image
           src={ctaBackground}
@@ -17,7 +16,6 @@ export default function CTA() {
         />
       </div>
 
-      {/* 텍스트 및 버튼 영역 */}
       <div className="relative bg-white pb-6 pt-9 px-3">
         <div className="pb-9 flex flex-col gap-1 items-center justify-center">
           <p className="font-bold text-2xl text-gray-900">여행 좋아하시나요?</p>
@@ -26,12 +24,18 @@ export default function CTA() {
           </p>
         </div>
         <div className="flex flex-col gap-2 items-center justify-center w-full">
-          <Button color="green" variants="primary" className="w-full py-3.5">
+          <Link
+            href="/map"
+            className="w-full py-3.5 rounded-xl border bg-primary-300 hover:bg-primary-400 border-primary-300 hover:border-primary-400 text-gray-0 text-sm font-semibold text-center transition"
+          >
             시작하기
-          </Button>
-          <Button color="green" variants="ghost" className="w-full py-3.5">
+          </Link>
+          <Link
+            href="/"
+            className="w-full py-3.5 rounded-xl border bg-gray-0 hover:bg-primary-50 border-gray-0 hover:border-primary-50 text-gray-700 hover:text-primary-400 text-sm font-semibold text-center transition"
+          >
             괜찮아요
-          </Button>
+          </Link>
         </div>
       </div>
     </div>
