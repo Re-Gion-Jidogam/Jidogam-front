3c535fc210abeb6d75d42357f05d20b601939cef
jieunsse <jieunsse@gmail.com>
2026-03-25 20:47:44 +0900

    Chore: 경로변경 반영

--- src/app/map/guidebook/page.tsx
+++ src/app/map/guidebook/page.tsx
@@ -8,11 +8,11 @@ import SearchBar from "@/components/SearchBar";
 import SegmentGroup from "@/components/SegmentGroup";
 import ToggleGroup from "@/components/ToggleGroup";
 
-import BannerCard from "./_components/BannerCard";
-import GuidebookCreateSheet from "./_components/Guidebook/GuidebookCreateSheet";
-import BannerHeart from "./_assets/banner-heart.svg";
-import BannerStar from "./_assets/banner-star.svg";
-import { MOCK_MY_GUIDEBOOKS } from "./_mocks/guidebooks";
+import BannerCard from "./_domain/components/BannerCard";
+import GuidebookCreateSheet from "./_domain/components/Guidebook/GuidebookCreateSheet";
+import BannerHeart from "./_domain/assets/banner-heart.svg";
+import BannerStar from "./_domain/assets/banner-star.svg";
+import { MOCK_MY_GUIDEBOOKS } from "./_domain/mocks/guidebooks";
 
 const FILTER_ITEMS = [
   { id: "published", label: "출판됨", image: "ToggleGroupCheck" as const },
